fd385343d43e78905eb6cbbe48e855394d1f60f0
Gonzalo <Gonzalo-Alberto@outlook.com>
2017-10-19 12:06:32 -0400

    no message

---
+++ Claymore Dual Miner Config/MainWindow.xaml.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.IO.Compression;
+using IWshRuntimeLibrary;
+
+namespace Claymore_Dual_Miner_Config
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            if (RdNone.IsChecked == true)
+            {
+                txtAltAddress.IsEnabled = false;
+            }
+        }
+
+        private void BtnCreateFiles_Click(object sender, RoutedEventArgs e)
+        {
+            string path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            string localPath = new Uri(path).LocalPath;
+            string tempFolder = Environment.GetEnvironmentVariable("temp");
+            string claymoreTempLoc = tempFolder + @"\Claymore_s_Dual_Ethereum_Decred_Siacoin_Lbry_Pascal_AMD_NVIDIA_GPU_Miner_v10_0.zip";
+            string claymorePath = localPath + @"\Claymore_s_Dual_Ethereum_Decred_Siacoin_Lbry_Pascal_AMD_NVIDIA_GPU_Miner_v10_0";
+
+
+            OutputText createFiles = new OutputText();
+
+            // UNZIPS CLAYMORE'S FILES TO LOCATION OF THIS EXECUTABLE
+            if (System.IO.File.Exists(claymorePath + @"\Readme!!!.txt") == false)
+            {
+                System.IO.File.WriteAllBytes(claymoreTempLoc, Properties.Resources.Claymore_s_Dual_Ethereum_Decred_Siacoin_Lbry_Pascal_AMD_NVIDIA_GPU_Miner_v10_0);
+                ZipFile.ExtractToDirectory(claymoreTempLoc, claymorePath);
+            }
+
+           if (RdETH.IsChecked == true | RdETC.IsChecked == true)
+            {
+                createFiles.StartBatch(claymorePath, CoinType(), altCoinType(), port(), altPort(), TxtMainAddress.Text, txtAltAddress.Text, TxtName.Text, TxtEmail.Text);
+                createFiles.ePoolTextFile(claymorePath, CoinType(), port(), TxtMainAddress.Text, TxtName.Text, TxtEmail.Text);
+            }
+
+           if (RdSIA.IsChecked == true)
+            {
+                createFiles.dPoolTextFileSIA(claymorePath, altPort(), txtAltAddress.Text, TxtMainAddress.Text, TxtName.Text, TxtEmail.Text);
+            }
+
+           if (RdPASC.IsChecked == true)
+            {
+                createFiles.dPoolPASC(claymorePath, altCoinType(),altPort(), TxtMainAddress.Text, TxtName.Text, TxtEmail.Text);
+            }
+
+           // AUTO START ENABLED BY DEFAULT
+            if (ChkboxAutoStart.IsChecked == true)
+            {
+                string appData = Environment.GetEnvironmentVariable("AppData");
+                var startupFolderPath = appData + @"\Microsoft\Windows\Start Menu\Programs\Startup";
+
+                var shell = new WshShell();
+                var shortCutLinkFilePath = startupFolderPath + @"\start.lnk";
+
+                var windowsApplicationShortcut = (IWshShortcut)shell.CreateShortcut(shortCutLinkFilePath);
+                windowsApplicationShortcut.Description = "How to create short for application example";
+                windowsApplicationShortcut.WorkingDirectory = claymorePath;
+                windowsApplicationShortcut.TargetPath = claymorePath + @"\start.bat";
+                windowsApplicationShortcut.Save();
+            }
+
+            // RETURNS TYPE OF COIN
+            string CoinType()
+            {
+                //string[] typeOfCoin = new string[4] { "eth", "etc", "sia", "pasc" };
+                string type = "";
+
+                if (RdETH.IsChecked == true)
+                {
+                    type = "eth";
+                }
+
+                if (RdETC.IsChecked == true)
+                {
+                    type = "etc";
+                }                
+                return type;
+            }
+
+            // RETURNS ALT COIN TYPE
+            string altCoinType()
+            {                
+                string type = "";
+
+                if (RdSIA.IsChecked == true)
+                {
+                    type = "sia";
+                }
+
+                if (RdPASC.IsChecked == true)
+                {
+                    type = "pasc";
+                }
+                return type;
+            }
+
+            // RETURNS PORT NUMBER
+            string port()
+            {
+                string portNumber = "";
+
+                if (RdETH.IsChecked == true)
+                {
+                    portNumber = "9999";
+                }
+
+                if (RdETC.IsChecked == true)
+                {
+                    portNumber = "19999";
+                }
+                return portNumber;
+            }
+
+            string altPort()
+            {
+                string portNumber = "";
+
+                if (RdSIA.IsChecked == true)
+                {
+                    portNumber = "7777";
+                }
+
+                if (RdPASC.IsChecked == true)
+                {
+                    portNumber = "15555";
+                }
+                return portNumber;
+            }
+
+        }
+
+        private void RdSIA_Checked(object sender, RoutedEventArgs e)
+        {
+            txtAltAddress.IsEnabled = true;
+        }
+
+        private void RdPASC_Checked(object sender, RoutedEventArgs e)
+        {
+            txtAltAddress.IsEnabled = true;
+        }
+
+        private void RdNone_Checked(object sender, RoutedEventArgs e)
+        {
+            txtAltAddress.IsEnabled = false;
+        }
+
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }        
+    }
+}
---
+++ Claymore Dual Miner Config/OutputText.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Claymore_Dual_Miner_Config
+{
+    class OutputText
+    {
+        string[] servers = new string[4] { "us-west1", "eu1", "eu2", "asia1" };
+
+        //START BATCH FILE SINGLE MINING ONLY
+        public void StartBatch(string claymorePath, string coinType, string altCoinType, string port, string altPort, string mainAddress, string altAddress, string compName, string email)
+        {
+            // WRITES THE START BATCH FILE
+            if (coinType == "eth" | coinType == "etc")
+            {
+                using (StreamWriter outputStartFile = new StreamWriter(claymorePath + @"\start.bat"))
+                {
+                    outputStartFile.WriteLine(
+                        "TIMEOUT /T 15" + Environment.NewLine +
+                        " " + Environment.NewLine +
+                        "setx GPU_FORCE_64BIT_PTR 0" + Environment.NewLine + 
+                        "setx GPU_MAX_HEAP_SIZE 100" + Environment.NewLine +
+                        "setx GPU_USE_SYNC_OBJECTS 1" + Environment.NewLine +
+                        "setx GPU_MAX_ALLOC_PERCENT 100" + Environment.NewLine +
+                        "setx GPU_SINGLE_ALLOC_PERCENT 100" + Environment.NewLine +
+                        " " + Environment.NewLine +
+                        "EthDcrMiner64.exe -epool " + coinType + "-us-east1.nanopool.org:" + port + " -ewal " + mainAddress + "." + compName + "/" + email + " -epsw x -mode 1 -ftime 10"
+                        );
+                }
+            }
+            
+            if (altCoinType == "sia")
+            {
+                using (StreamWriter outputStartFile = new StreamWriter(claymorePath + @"\start.bat"))
+                {
+                    outputStartFile.WriteLine(
+                        "TIMEOUT /T 15 " + Environment.NewLine +
+                        " " + Environment.NewLine +
+                        "setx GPU_FORCE_64BIT_PTR 0" + Environment.NewLine +
+                        "setx GPU_MAX_HEAP_SIZE 100" + Environment.NewLine +
+                        "setx GPU_USE_SYNC_OBJECTS 1" + Environment.NewLine +
+                        "setx GPU_MAX_ALLOC_PERCENT 100" + Environment.NewLine +
+                        "setx GPU_SINGLE_ALLOC_PERCENT 100" + Environment.NewLine +
+                        " " + Environment.NewLine +
+                        "EthDcrMiner64.exe -epool " + coinType + "-us-east1.nanopool.org:" + port + " -ewal " + mainAddress + "." + compName + "/" + email + 
+                        " -epsw x -dcoin sia -dpool stratum+tcp://sia-us-east1.nanopool.org:" + altPort + " -dwal " + altAddress + "/" + compName + "/" + email + " -dpsw x -ftime 10"
+                        );
+                }
+            }
+
+            //if (altCoinType == "pasc")
+            //{
+            //    using (StreamWriter outputStartFile = new StreamWriter(claymorePath + @"\start.bat"))
+            //    {
+            //        outputStartFile.WriteLine(
+            //            "TIMEOUT /T 15 " + Environment.NewLine +
+            //            " " + Environment.NewLine +
+            //            "setx GPU_FORCE_64BIT_PTR 0" + Environment.NewLine +
+            //            "setx GPU_MAX_HEAP_SIZE 100" + Environment.NewLine +
+            //            "setx GPU_USE_SYNC_OBJECTS 1" + Environment.NewLine +
+            //            "setx GPU_MAX_ALLOC_PERCENT 100" + Environment.NewLine +
+            //            "setx GPU_SINGLE_ALLOC_PERCENT 100" + Environment.NewLine +
+            //            " " + Environment.NewLine +
+            //            "EthDcrMiner64.exe -epool " + coinType + "-us-east1.nanopool.org:" + port + " -ewal " + mainAddress + "." + compName + "/" + email + 
+            //            " -epsw x -dcoin pasc -dpool pasc-us-east.nanopool.org:15555 -dwal " + altAddress + "." + compName + "/" + email + " -dpsw x -mode 0 -ftime 10"
+            //            );
+            //    }
+            //}
+        }
+
+
+        // CREATES EPOOL TEXT FILE
+        public void ePoolTextFile(string claymorePath, string coinType, string port, string mainAddress, string compName, string email)
+        {            
+            using (StreamWriter outputTextFile = new StreamWriter(claymorePath + @"\epools.txt"))
+            {             
+                foreach (string server in servers)
+                {
+                    outputTextFile.WriteLine(
+                        "POOL: " + coinType + "-" + server + ".nanopool.org:" + port + ", WALLET: " + mainAddress + "." + compName + "/" + email + ", PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0"
+                        );
+                }
+            }
+        }
+
+        // CREATES DPOOL TEXT FILE FOR SIA
+        public void dPoolTextFileSIA(string claymorePath, string altPort, string altAddress, string mainAddress, string compName, string email)
+        {
+            using (StreamWriter outputTextFile = new StreamWriter(claymorePath + @"\dpools.txt"))
+            {
+                foreach (string server in servers)
+                {
+                    outputTextFile.WriteLine(
+                        "POOL: stratum+tcp://sia-" + server + ".nanopool.org:" + altPort + ", WALLET: " + altAddress + "/" + compName + "/" + email + ", PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0"
+                        );
+                }
+            }
+        }
+
+        // CREATES DPOOL TEXT FILE FOR SIA
+        public void dPoolPASC(string claymorePath, string altCoinType, string altPort, string mainAddress, string compName, string email)
+        {
+            using (StreamWriter outputTextFile = new StreamWriter(claymorePath + @"\dpools.txt"))
+            {
+                foreach (string server in servers)
+                {
+                    outputTextFile.WriteLine(
+                        "POOL: " + altCoinType + "-" + server + ".nanopool.org:" + altPort + ", WALLET: " + mainAddress + "." + compName + "/" + email + ", PSW: x, WORKER: , ESM: 0, ALLPOOLS: 0"
+                        );
+                }
+            }
+        }
+    }
+}
